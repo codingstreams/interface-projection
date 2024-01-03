44cf9bcab416e1de731fbb82b4f0b7976d7571fd
CodingStreams <akxhdev@outlook.com>
2024-01-03 18:11:38 +0530

    Initial Commit

---
+++ src/main/java/com/codingstreams/interfaceprojection/InterfaceProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.codingstreams.interfaceprojection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceProjectionApplication.class, args);
+	}
+
+}
